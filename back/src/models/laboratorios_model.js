56c285f0b3273407ec9bb64e74fbcfdf2a0ee2ce
Pablo Berdasco <pberdasco@gmail.com>
2025-01-30 09:02:57 -0300

    formato laboratorio string 10

--- back/src/models/laboratorios_model.js
+++ back/src/models/laboratorios_model.js
@@ -2,7 +2,7 @@ export default class laboratorio{
     id;          // int
     nombre;      // string(45)
     domicilio;   // string(60)
-    formato;     // string(60)
+    formato;     // string(10)
 
     constructor(row){
         this.id = row.id;
--- back/src/models/laboratorios_schema.js
+++ back/src/models/laboratorios_schema.js
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const laboratorioCreateSchema = z.object({
     nombre: z.string().max(45),
     domicilio: z.string().max(60),
-    formato: z.string().max(60)
+    formato: z.string().max(10)
 });
 
 export const laboratorioUpdateSchema = laboratorioCreateSchema.partial();
