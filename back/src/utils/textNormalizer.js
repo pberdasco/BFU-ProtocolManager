9bbc40cb39c2c3ee5235486292d05562e91cb030
Pablo Berdasco <pberdasco@gmail.com>
2025-06-09 17:16:51 -0300

    fix: Update punctuation removal regex in text normalization utility to include additional characters

--- back/src/utils/textNormalizer.js
+++ back/src/utils/textNormalizer.js
@@ -2,7 +2,7 @@ export function normalizarTexto (texto) {
     return texto
         .normalize('NFD')
         .replace(/[\u0300-\u036f]/g, '') // eliminar tildes
-        .replace(/[.,/#!$%^&*;:{}=\-_`~()[\]]/g, '') // eliminar signos de puntuación
+        .replace(/[.,/#!$%^&*;:{}=\-_`~()[\]+–]/g, '') // eliminar signos de puntuación
         .replace(/\s+/g, '') // eliminar espacios
         .toLowerCase(); // pasar a minúsculas
 }
