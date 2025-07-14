66093fa99895c8897b2f10a065522c4e8352e229
Pablo Berdasco <pberdasco@gmail.com>
2025-07-14 18:48:48 -0300

    ajustes front: conversion UM en nivel guia - tablasCDI

---
+++ public/assets/subproyectos-r_y-EKFw.js
@@ -0,0 +1,1 @@
+import{u as t}from"./useQuery-DA5bnrbd.js";import{a as c}from"./apiResource-5P-thon0.js";const u=async()=>(await new c("subproyectos").fetch({sort:[{selector:"codigo",desc:!1}]})).data,p=()=>{const{data:o=[],isLoading:e,error:r,...s}=t({queryKey:["subproyectos"],queryFn:u});return{subproyectos:o,isLoadingSubproyectos:e,errorSubproyecto:r,...s}};export{p as u};
